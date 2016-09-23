ae035a0a441d96d8c52b7363d62ec1fb707722f6
Demian Sciessere <demian711@hotmail.com>
2016-09-23 18:33:12 -0300

    tabla clientes en mongoose

--- modules/clients/server/models/client.server.model.js
+++ modules/clients/server/models/client.server.model.js
@@ -6,6 +6,11 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
+var validateCuil = function(cuil) {
+    var re = /^[0-9]{10,11}$/;
+    return re.test(cuil)
+};
+
 /**
  * Client Schema
  */
@@ -16,10 +21,48 @@ var ClientSchema = new Schema({
     required: 'Please fill Client name',
     trim: true
   },
+  business_name: {
+    type: String,
+    default: '',
+    required: 'Please fill Client business name',
+    trim: true
+  },
+  cuil: {
+    type: Number,
+    default: '',
+    required: 'Please fill Client cuil',
+    trim: true,
+    validate: [validateCuil, 'Por favor ingrese un numero de cuil valido'],
+        match: [/^[0-9]{10,11}$/, 'Por favor ingrese un numero de cuil valido']
+  },
+  address: {
+    type: String,
+    default: '',
+    required: 'Please fill Client address',
+    trim: true
+  },
   email: {
     type: String,
     default: '',
     required: 'Please fill Client email',
+    trim: true
+  },
+  phone: {
+    type: Number,
+    default: '',
+    required: 'Please fill Client Phone number',
+    trim: true
+  },
+  cellphone: {
+    type: Number,
+    default: '',
+    required: 'Please fill Client Cellphone number',
+    trim: true
+  },
+  fax: {
+    type: Number,
+    default: '',
+    required: 'Please fill Client Fax number',
     trim: true
   },
   created: {
